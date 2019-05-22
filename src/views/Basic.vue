5f1ad2f956fc244c8624ec509e4211e320eb6c66
timtnleeProject <litingen1995@gmail.com>
2019-05-22 11:20:02 +0800

    0.2.13-beta.1

--- src/views/Basic.vue
+++ src/views/Basic.vue
@@ -1,114 +1,189 @@
 <template>
-<div>
+  <div>
     <div class="popup" v-if="popup">
-        <div class="popup-content">
-            <div class="flow">
-            <loader :done="done">Converting</loader>
-            <img :src="clipResult" class="img">
+      <div class="popup-content">
+        <div class="flow">
+          <loader :done="done">Converting</loader>
+          <img :src="clipResult" class="img">
+          <div class="flex-center">
             <div class="flex-center">
-                <div class="flex-center">
-                    <input type="text" v-model="filename">
-                    <span>.png</span>
-                </div>
+              <input type="text" v-model="filename">
+              <span>.png</span>
             </div>
-            <div class="flex-center">
-                <button class="btn g"><a :href="link" :download="filename">download</a></button>
-                <button class="btn r" @click="()=>{this.popup = false}">close</button>
-            </div>
-            </div>
-            <img src="icon/cancel.png" alt="" class="close" @click="()=>{this.popup = false}">
+          </div>
+          <div class="flex-center">
+            <button class="btn g">
+              <a :href="link" :download="filename">download</a>
+            </button>
+            <button class="btn r" @click="()=>{this.popup = false}">close</button>
+          </div>
         </div>
+        <img src="icon/cancel.png" alt class="close" @click="()=>{this.popup = false}">
+      </div>
     </div>
     <div class="content">
-        <h2>clipper-basic</h2>
-        <div class="area">
-            <ul>
-                <li>Zoom and drag clipping area to clip the image.</li>
-                <li>It is recommended to use it to clip image without ratio.</li>
-                <li>Supported rotate, scale and ratio but they do not default features.</li>
-            </ul>
-        </div>
-        <h4>gallary</h4>
-        <gallary :gallary="gallary" v-model="imgUrl"></gallary>
-        <h4>demo</h4>
-        <clipper-basic class="basic-sample" ref="clipper" :style="basicStyle" @load="imgLoad" :src="imgUrl" :outline="outline||0" :border="border" :rotate="rotate" :ratio="ratio" :scale="scale" :corner="corner" :grid="grid" :bgColor="bgColor" :shadow="shadow" :mode="mode" preview="my-preview">
-            <div slot="placeholder">No image uploaded</div>
-        </clipper-basic>
-        <div class="flex-center">
-            <div class="settings">
-                <h4>settings</h4>
-                <div class="flex">
-                    <button @click="clip" class="btn">clip image</button>
-                    <div>
-                      <clipper-upload class="btn" v-model="imgUrl">upload image</clipper-upload>
-                    </div>
-                </div>
-                    <div class="flex">
-                        <div class="flex">
-                            <span class="label">outline</span>
-                            <span><input v-model.number="outline" type="number" min="1"></span>
-                        </div>
-                            <div class="flex">
-                                <span  class="label">border</span>
-                                <span><input v-model.number="border" type="number" min="1"></span>
-                            </div>
-                            </div>
-                            <div class="flex">
-                                <span  class="label">layout: </span>
-                                <span><input type="checkbox" name="corner" v-model="corner">corner</span>
-                                <span><input type="checkbox" name="grid" v-model="grid">grid</span>
-                            </div>
-                            <div class="flex">
-                                <span class="label">ratio: </span>
-                                <span><input type="radio" name="ratio" :value="0" v-model.number="ratio">no ratio</span>
-                                <span><input type="radio" name="ratio" :value="1" v-model.number="ratio">1:1</span>
-                                <span><input type="radio" name="ratio" :value="4/3" v-model.number="ratio">4:3</span>
-                                <span><input type="radio" name="ratio" :value="16/9" v-model.number="ratio">16:9</span>
-                            </div>
-                            <div class="flex">
-                                <span class="label">mode: </span>
-                                <span><input type="radio" name="mode" value="normal" v-model="mode">normal</span>
-                                <span><input type="radio" name="mode" value="switch" v-model="mode">switch</span>
-                            </div>
-                            <div class="flex">
-                                <span class="label">bgColor: </span>
-                                <span><input type="radio" name="bgColor" value="#000000" v-model="bgColor"><span class="color-tick" style="background-color:#000000"></span></span>
-                                <span><input type="radio" name="bgColor" value="#d8adad" v-model="bgColor"><span class="color-tick" style="background-color:#d8adad"></span></span>
-                                <span><input type="radio" name="bgColor" value="#a1cfea" v-model="bgColor"><span class="color-tick" style="background-color:#a1cfea"></span></span>
-                                <span><input type="radio" name="bgColor" value="#bc1229" v-model="bgColor"><span class="color-tick" style="background-color:#bc1229"></span></span>
-                            </div>
-                            <div class="flex">
-                                <span class="label">shadow: </span>
-                                <span><input type="radio" name="shadow" value="rgba(0,0,0,0.4)" v-model="shadow"><span class="shadow-tick" style="background-color:rgba(0,0,0,0.4)"></span></span>
-                                <span><input type="radio" name="shadow" value="rgba(0,0,0,0.8)" v-model="shadow"><span class="shadow-tick" style="background-color:rgba(0,0,0,0.8)"></span></span>
-                                <span><input type="radio" name="shadow" value="rgba(255,255,255,0.2)" v-model="shadow"><span class="shadow-tick" style="background-color:rgba(255,255,255,0.2)"></span></span>
-                                <span><input type="radio" name="shadow" value="rgb(27, 170, 232,0.5)" v-model="shadow"><span class="shadow-tick" style="background-color:rgb(27, 170, 232,0.5)"></span></span>
-                            </div>
-                            <div class="flex">
-                                <span class="w80 label">rotate: </span>
-                                <clipper-range class="range" :min="0" :max="360" v-model="rotate"></clipper-range>
-                            </div>
-                            <div class="flex">
-                                <span class="w80 label">scale: </span>
-                                <clipper-range class="range" :min="0.2" :max="4" v-model="scale"></clipper-range>
-                            </div>
-                        </div>
-                        <div class="previews">
-                            <h4>preview</h4>
-                            <div class="img-grid">
-                                <clipper-preview class="preview preview-lg" name="my-preview"></clipper-preview>
-                                <clipper-preview class="preview preview-md" name="my-preview"></clipper-preview>
-                                <clipper-preview class="preview preview-sm" name="my-preview"></clipper-preview>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <h2>clipper-basic</h2>
+      <div class="area">
+        <ul>
+          <li>Zoom and drag clipping area to clip the image.</li>
+          <li>It is recommended to use it to clip image without ratio.</li>
+          <li>Supported rotate, scale and ratio but they do not default features.</li>
+        </ul>
+      </div>
+      <h4>gallary</h4>
+      <gallary :gallary="gallary" v-model="imgUrl"></gallary>
+      <h4>demo</h4>
+      <clipper-basic
+        class="basic-sample"
+        ref="clipper"
+        :style="basicStyle"
+        @load="imgLoad"
+        :src="imgUrl"
+        :outline="outline||0"
+        :border="border"
+        :minWidth="minWidth"
+        :minHeight="minHeight"
+        :rotate="rotate"
+        :ratio="ratio"
+        :scale="scale"
+        :corner="corner"
+        :grid="grid"
+        :bgColor="bgColor"
+        :shadow="shadow"
+        :mode="mode"
+        preview="my-preview"
+      >
+        <div slot="placeholder">No image uploaded</div>
+      </clipper-basic>
+      <div class="flex-center">
+        <div class="settings">
+          <h4>settings</h4>
+          <div class="flex">
+            <button @click="clip" class="btn">clip image</button>
+            <div>
+              <clipper-upload class="btn" v-model="imgUrl">upload image</clipper-upload>
             </div>
+          </div>
+          <div class="flex">
+            <div class="flex">
+              <span class="label">outline</span>
+              <span>
+                <input v-model.number="outline" type="number" min="1">
+              </span>
+            </div>
+            <div class="flex">
+              <span class="label">border</span>
+              <span>
+                <input v-model.number="border" type="number" min="1">
+              </span>
+            </div>
+          </div>
+          <div class="flex">
+            <span class="label">minWidth(%)</span>
+            <span>
+              <input v-model.number="minWidth" type="number" min="1" max="50">
+            </span>
+            <span class="label">minHeight(%)</span>
+            <span>
+              <input v-model.number="minHeight" type="number" min="1" max="50">
+            </span>
+          </div>
+          <div class="flex">
+            <span class="label">layout:</span>
+            <span>
+              <input type="checkbox" name="corner" v-model="corner">corner
+            </span>
+            <span>
+              <input type="checkbox" name="grid" v-model="grid">grid
+            </span>
+          </div>
+          <div class="flex">
+            <span class="label">ratio:</span>
+            <span>
+              <input type="radio" name="ratio" :value="0" v-model.number="ratio">no ratio
+            </span>
+            <span>
+              <input type="radio" name="ratio" :value="1" v-model.number="ratio">1:1
+            </span>
+            <span>
+              <input type="radio" name="ratio" :value="4/3" v-model.number="ratio">4:3
+            </span>
+            <span>
+              <input type="radio" name="ratio" :value="2/3" v-model.number="ratio">2:3
+            </span>
+          </div>
+          <div class="flex">
+            <span class="label">mode:</span>
+            <span>
+              <input type="radio" name="mode" value="normal" v-model="mode">normal
+            </span>
+            <span>
+              <input type="radio" name="mode" value="switch" v-model="mode">switch
+            </span>
+          </div>
+          <div class="flex">
+            <span class="label">bgColor:</span>
+            <span>
+              <input type="radio" name="bgColor" value="#000000" v-model="bgColor">
+              <span class="color-tick" style="background-color:#000000"></span>
+            </span>
+            <span>
+              <input type="radio" name="bgColor" value="#d8adad" v-model="bgColor">
+              <span class="color-tick" style="background-color:#d8adad"></span>
+            </span>
+            <span>
+              <input type="radio" name="bgColor" value="#a1cfea" v-model="bgColor">
+              <span class="color-tick" style="background-color:#a1cfea"></span>
+            </span>
+            <span>
+              <input type="radio" name="bgColor" value="#bc1229" v-model="bgColor">
+              <span class="color-tick" style="background-color:#bc1229"></span>
+            </span>
+          </div>
+          <div class="flex">
+            <span class="label">shadow:</span>
+            <span>
+              <input type="radio" name="shadow" value="rgba(0,0,0,0.4)" v-model="shadow">
+              <span class="shadow-tick" style="background-color:rgba(0,0,0,0.4)"></span>
+            </span>
+            <span>
+              <input type="radio" name="shadow" value="rgba(0,0,0,0.8)" v-model="shadow">
+              <span class="shadow-tick" style="background-color:rgba(0,0,0,0.8)"></span>
+            </span>
+            <span>
+              <input type="radio" name="shadow" value="rgba(255,255,255,0.2)" v-model="shadow">
+              <span class="shadow-tick" style="background-color:rgba(255,255,255,0.2)"></span>
+            </span>
+            <span>
+              <input type="radio" name="shadow" value="rgb(27, 170, 232,0.5)" v-model="shadow">
+              <span class="shadow-tick" style="background-color:rgb(27, 170, 232,0.5)"></span>
+            </span>
+          </div>
+          <div class="flex">
+            <span class="w80 label">rotate:</span>
+            <clipper-range class="range" :min="0" :max="360" v-model="rotate"></clipper-range>
+          </div>
+          <div class="flex">
+            <span class="w80 label">scale:</span>
+            <clipper-range class="range" :min="0.2" :max="4" v-model="scale"></clipper-range>
+          </div>
+        </div>
+        <div class="previews">
+          <h4>preview</h4>
+          <div class="img-grid">
+            <clipper-preview class="preview preview-lg" name="my-preview"></clipper-preview>
+            <clipper-preview class="preview preview-md" name="my-preview"></clipper-preview>
+            <clipper-preview class="preview preview-sm" name="my-preview"></clipper-preview>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import clipToURL from '@/clip.js';
-import Gallary from '@/components/Gallary.vue';
+import clipToURL from '@/clip.js'
+import Gallary from '@/components/Gallary.vue'
 export default {
   components: {
     Gallary
@@ -122,6 +197,8 @@ export default {
       popup: false,
       ratio: 0,
       rotate: 0,
+      minWidth: 1,
+      minHeight: 1,
       scale: 1,
       outline: 10,
       border: 1,
@@ -135,7 +212,7 @@ export default {
       filename: 'clip',
       done: false,
       maxWidth: 850,
-      gallary: ['dawn.jpg','long.jpg','ex1.jpg','profile.png']
+      gallary: ['dawn.jpg', 'long.jpg', 'ex1.jpg', 'profile.png']
     };
   },
   methods: {
@@ -148,10 +225,10 @@ export default {
     imgLoad: function() {
       const maxHeight = 500;
       const imgRatio = this.$refs.clipper.imgRatio;
-      if (imgRatio < 1) this.maxWidth = maxHeight * imgRatio;
+      if (!this.ratio && imgRatio < 1) this.maxWidth = maxHeight * imgRatio;
       else this.maxWidth = 700;
-      if (!this.gallary.find(g=>g===this.imgUrl))
-        this.gallary.push(this.imgUrl)
+      if (!this.gallary.find(g => g === this.imgUrl))
+        this.gallary.push(this.imgUrl);
     }
   },
   computed: {
